31c9e2a6ff54fdb11e84db84f42c549a2e573351
koudai001 <k53612796@gmail.com>
2026-09-15 11:39:24 +0900

    update

--- src/auth.ts
+++ src/auth.ts
@@ -2,11 +2,11 @@ import { env } from "cloudflare:workers";
 import { betterAuth } from "better-auth";
 import { jwt } from "better-auth/plugins";
 import { drizzleAdapter } from "@better-auth/drizzle-adapter/relations-v2";
-import { drizzle } from "drizzle-orm/d1";
+import { db } from "./db";
 import * as schema from "./db/schema";
 
 export const auth = betterAuth({
-  database: drizzleAdapter(drizzle(env.DB), {
+  database: drizzleAdapter(db, {
     provider: "sqlite",
     schema: schema,
   }),
